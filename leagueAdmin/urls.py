cf8c0f9e28dd4cae48065ee150d04993a3184e22
PatrykCzartowski <patrykczartowski6@gmail.com>
2023-06-10 18:48:42 +0200

    10.06.2023

--- leagueAdmin/urls.py
+++ leagueAdmin/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('games/',views.games, name="games"),
     path('players/',views.players, name="players"),
     path('login/',views.log_in , name="login"),
-    path('player_add/',views.player_add, name="player_add")
+    path('player_add/',views.player_add, name="player_add"),
+    path('player_delete/',views.player_delete, name="player_delete"),
+    path('player_update/',views.player_update, name="player_update"),
+    path('game_add/',views.game_add, name="game_add"),
+    path('game_delete/',views.game_delete, name="game_delete"),
+    path('game_update/',views.game_update, name="game_update")
 ]
--- leagueAdmin/views.py
+++ leagueAdmin/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Players, Games
 
-# Create your views here.
-
+#--------------------------------#
+# Main pages rendering:
+#--------------------------------#
 def index(request):
     all_players = Players.objects.all
     all_games = Games.objects.all
@@ -18,24 +19,91 @@ def players(request):
     player = Players.objects.all
     return render(request,'players.html/', {'all': player})
 
+#--------------------------------#
+# Login page rendering and validation:
+#--------------------------------#
 def log_in(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            return render(request, 'games.html')
+            return games(request)
     else:
         return render(request, 'login.html')
     
+#--------------------------------#
+#Admins - player CRUD operations:
+#--------------------------------#
+
+# Player add form rendering and saving in the database:
 def player_add(request):
-    return render(request,'players_add.html/',)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        ranking = request.POST.get('ranking')
+        new_player = Players(name=name, surname=surname, ranking=ranking)
+        new_player.save()
+        
+    return render(request,'player_add.html',)
 
+# Player delete form rendering and deleting from the database:
 def player_delete(request):
-    return render(request,'players_delete.html/',)
+    if request.method == 'POST':
+        rm_id = request.POST.get('ID')
+        try:
+            ID = Players.objects.get(id=rm_id)
+            ID.delete() 
+        except:
+            return players(request)
+    return render(request,'player_delete.html',)
 
+# Player update form rendering and updating the database:
 def player_update(request):
-    return render(request,'players_update.html/',)
+    if request.method == 'POST':
+        up_id = request.POST.get('ID')
+        new_ranking = request.POST.get('ranking')
+        try:
+            player = Players.objects.get(id=up_id)
+            player.ranking = new_ranking
+            player.save()
+        except:
+            return players(request)
+    return render(request,'player_update.html',)
 
-def games_add(request):
-    return render(request,'games_add.html/',)
+# Game add form rendering and saving in the database:
+def game_add(request):
+    if request.method == 'POST':
+        players = request.POST.get('players')
+        result = request.POST.get('result')
+        if result == '1:0' or result == '1:1' or result == '0:1':
+            new_game = Games(players=players, result=result)
+            new_game.save()
+        else:
+            return games(request)
+    
+    return render(request,'game_add.html',)
+
+# Game delete form rendering and deleting from the database:
+def game_delete(request):
+    if request.method == 'POST':
+        rm_id = request.POST.get('ID')
+        try:
+            ID = Games.objects.get(id=rm_id)
+            ID.delete() 
+        except:
+            return games(request)
+    return render(request,'game_delete.html',)
+
+# Game update form rendering and updating the database:
+def game_update(request):
+    if request.method == 'POST':
+        up_id = request.POST.get('ID')
+        new_result = request.POST.get('result')
+        try:
+            game = Games.objects.get(id=up_id)
+            game.result = new_result
+            game.save()
+        except:
+            return games(request)
+    return render(request,'game_update.html',)
