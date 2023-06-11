2ec4d63f1a75f1f3906bff29e2d36d0994d32fc5
PatrykCzartowski <patrykczartowski6@gmail.com>
2023-06-11 11:23:47 +0200

    11.06.2023

--- Chess_League/settings.py
+++ Chess_League/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Chess_League.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chess_league',
+        'USER': 'ladmin',
+        'PASSWORD': 'ladmin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
